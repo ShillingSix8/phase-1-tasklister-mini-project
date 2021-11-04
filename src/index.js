758bfa8ead676d67dcfc5b6ad788c8f45083dab5
benjamin <benjinoble@gmail.com>
2021-11-04 15:46:31 +1100

    Attempted Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+// Got cooked, tried to follow the solution ???
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const taskForm = document.querySelector(`#create-task-form`);
+  const taskDescription = document.querySelector(`#new-task-description`);
+  const taskPriority = document.querySelector("#new-task-priority");
+
+  const taskList = document.querySelector("#tasks");
+
+  taskForm.addEventListener("submit", createTask);
 });
+
+const createTask = event => {
+  event.preventDefault();
+
+  const taskDescription = document.querySelector("#new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = taskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.querySelector("#tasks").appendChild(task);
+};
